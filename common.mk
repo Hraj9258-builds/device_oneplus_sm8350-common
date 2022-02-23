20c055e80d82242320d7b2c9c84beeb399898fde
Lup Gabriel <gwolf2u.website@gmail.com>
2022-02-24 01:20:27 +0530

    sm8350-common: Enable UDFPS icons and animations

--- common.mk
+++ common.mk
@@ -192,6 +192,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+TARGET_HAS_UDFPS := true
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
